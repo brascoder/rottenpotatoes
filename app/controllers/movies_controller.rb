dbc83b80078babeb3a20588c58124e97f0b89ce6
Ron Cooke <ron.cooke@me.com>
2014-05-26 17:28:41 -0400

    Add CRUD for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,28 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find params[:id]
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was created successfully."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes! params[:movie]
+    flash[:notice] = "#{@movie.title} was updated successfully."
+    redirect_to movie_path(@movie)
+  end
 end
