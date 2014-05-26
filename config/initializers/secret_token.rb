10ead7422e7b076e16a6e382c6c516c87f0e42ef
Ron Cooke <ron.cooke@me.com>
2014-05-26 07:37:49 -0400

    Regenerate app with Rails 3.2.15

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '3ca0efe317e84642da140b1294ce0e6aa4c0cb691bb148cf564a825602881a280187fcda5ba4cee7f828b0cb388db1b65de95d5ecec15d3bc576d19aba443cb9'
